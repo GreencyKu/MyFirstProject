9062e4c275ff55de05743fff6364c2c8aff1b249
GreencyKu <130981112+GreencyKu@users.noreply.github.com>
2023-04-19 14:47:20 +0800

    Update DoubleBeepers.py
    modify file

--- Standford_SC001_Project/DoubleBeepers.py
+++ Standford_SC001_Project/DoubleBeepers.py
@@ -1,6 +1,6 @@
 """
 File: DoubleBeepers.py
-Name:
+Name: Greency Ku
 -------------------------------
 TODO:
 """
@@ -12,7 +12,47 @@ def main():
     """
     Karel will double the beepers
     """
-    pass
+    move()
+    double_beepers()
+    put_back_beepers()
+    go_home()
+
+
+def double_beepers():
+    while on_beeper():
+        # old beepers, facing East
+        pick_beeper()
+        move()
+        put_beeper()
+        put_beeper()
+        # new beepers, facing East
+        turn_around()
+        move()
+        # old beepers, facing West
+        turn_around()
+
+
+def turn_around():
+    turn_left()
+    turn_left()
+
+
+def put_back_beepers():
+    move()
+    while on_beeper():
+        pick_beeper()
+        turn_around()
+        move()
+        put_beeper()
+        turn_around()
+        move()
+
+
+def go_home():
+    turn_around()
+    move()
+    move()
+    turn_around()
 
 
 # ----- DO NOT MODIFY CODE BELOW THIS LINE ----- #
